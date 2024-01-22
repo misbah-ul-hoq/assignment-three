e18c774d90a5d294cb89f4df9642e8de8f59dd13
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-01-22 23:36:00 +0600

    nav and slider section completed

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      "color-primary": "#FF4240",
+      "lightened-white": "rgba(255, 255, 255, 0.60)",
+    },
+  },
+};
